ec20728e1057ad465cdaee4808c5772d856c9239
daverose <davearose@gmail.com>
2016-04-04 22:19:49 -0600

    WIP

--- ict4315.unit2/src/Board.java
+++ ict4315.unit2/src/Board.java
@@ -2,12 +2,5 @@
  * Created by daverose on 4/2/16.
  */
 public class Board {
-    public static final int ROWS = 8;
-    public static final int COLUMNS = 8;
-
-  public static int[][] createBoard(){
-    int Board[][]  = new int[ROWS][COLUMNS];
-        return Board;
-    }
-
+    int[][] board = new int[8][8];
 }
--- ict4315.unit2/src/Game.java
+++ ict4315.unit2/src/Game.java
@@ -1,4 +1,4 @@
-import java.util.Random;
+import java.util.Arrays;
 
 /**
  * Created by daverose on 4/2/16.
@@ -7,46 +7,15 @@ public class Game {
     private static int currentRow;
     private static int currentCol;
     private static int moves = 0;
-    private static Random random = new Random();
-
-    private static int[][] GameBoard = Board.createBoard();
+    static Board GameBoard = new Board();
 
 
-    private static int play() {
-        int move = 0;
-        do {
-            ++move;
-            GameBoard[currentRow][currentCol] = move;
-        } while (nextMove());
-        return move;
-    }
-
-    private static boolean nextMove() {
-        int[] moves = {0, 0, 0, 0, 0, 0, 0, 0};
-        int position = 0;
-        for (int i = 0; i < Knight.MOVES; ++i) {
-            int r = currentRow + Knight.HORIZONTAL[i];
-            int c = currentCol + Knight.VERTICAL[i];
-            if (legalMove(r, c)) {
-                moves[position] = i;
-                ++position;
-            }
-        }
-
-        if (position > 0) {
-            int m = random.nextInt(position);
-            currentRow += Knight.HORIZONTAL[moves[m]];
-            currentCol += Knight.VERTICAL[moves[m]];
+     static boolean legalMove(final int row, final int col) {
+        if (row >= 0 && col >= 0 && row < GameBoard.board[0].length && col < GameBoard.board[2].length && GameBoard.board[row][col] == 0) {
+          System.out.println("Inside legalMove() and TRUE");
             return true;
         } else {
-            return false;
-        }
-    }
-
-    private static boolean legalMove(final int row, final int col) {
-        if (row >= 0 && col >= 0 && row < GameBoard[0].length && col < GameBoard[2].length && GameBoard[row][col] == 0) {
-            return true;
-        } else {
+         System.out.println("Inside legalMove() and FALSE");
             return false;
         }
     }
@@ -54,32 +23,48 @@ public class Game {
     private static boolean closedTour(final int beginRow, final int beginCol) {
         for (int i = 0; i < Knight.MOVES; ++i) {
             if (currentRow + Knight.HORIZONTAL[i] == beginRow && currentCol + Knight.VERTICAL[i] == beginCol) {
+                System.out.println("Inside closedTour and TRUE");
                 return true;
             }
         }
+        System.out.println("Inside closedTour and FALSE");
         return false;
     }
 
     public static void printBoard() {
-        for (int i = 0; i < GameBoard[0].length; ++i) {
-            for (int j = 0; j < GameBoard[1].length; ++j) {
-                System.out.printf("%2d ", GameBoard[i][j]);
+        for (int i = 0; i < GameBoard.board[0].length; ++i) {
+            for (int j = 0; j < GameBoard.board[1].length; ++j) {
+                System.out.printf("%2d ", GameBoard.board[i][j]);
+               // System.out.println("inside the J loop of print board");
             }
             System.out.println();
         }
     }
-
+    private static int play() {
+        int move = 0;
+        do {
+            ++move;
+            GameBoard.board[currentRow][currentCol] = move;
+        //  System.out.println(move);
+        } while (Knight.nextMove());
+       System.out.println("Move should return");
+        return move;
+    }
     public static void main(String[] args) {
         int count = 0;
         for (; count < 10000000; ++count) {
             currentRow = 0;
             currentCol = 0;
-            for (int i = 0; i < GameBoard[0].length; ++i) {
-                for (int j = 0; j < GameBoard[1].length; ++j) {
-                    GameBoard[i][j] = 0;
+            for (int i = 0; i < 8; ++i) {
+                for (int j = 0; j < 8; ++j) {
+                    GameBoard.board[i][j] = 0;
+                //   System.out.println(Arrays.toString(GameBoard.board[i]) + " " + Arrays.toString(GameBoard.board[j]));
                 }
             }
+            System.out.println("Before call to play()");
             moves = play();
+            System.out.println("After call to play()");
+            System.out.println(moves);
             if (moves == 64) {
                 if (closedTour(0, 0)) {
                     System.out.println("Closed Tour!");
@@ -89,8 +74,8 @@ public class Game {
                 printBoard();
                 break;
             }
-            if (count % 100000 == 0) {
-                System.out.println(count / 100000);
+            if (count % 1000 == 0) {
+                System.out.println(count / 1000);
             }
         }
         System.out.println("Trials: " + count);
