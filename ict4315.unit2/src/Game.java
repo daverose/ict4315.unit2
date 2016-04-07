703df2e66b6847c8d431604427dbd6bc939055ec
daverose <davearose@gmail.com>
2016-04-07 07:40:01 -0600

    rewrote all classes

--- ict4315.unit2/src/Game.java
+++ ict4315.unit2/src/Game.java
@@ -1,87 +1,37 @@
-import java.util.Arrays;
 
-/**
- * Created by daverose on 4/2/16.
- */
 public class Game {
-    private static int currentRow;
-    private static int currentCol;
-    private static int moves = 0;
-    static Board GameBoard = new Board();
-
-
-     static boolean legalMove(final int row, final int col) {
-        if (row >= 0 && col >= 0 && row < GameBoard.board[0].length && col < GameBoard.board[2].length && GameBoard.board[row][col] == 0) {
-          System.out.println("Inside legalMove() and TRUE");
-            return true;
-        } else {
-         System.out.println("Inside legalMove() and FALSE");
-            return false;
-        }
-    }
-
-    private static boolean closedTour(final int beginRow, final int beginCol) {
-        for (int i = 0; i < Knight.MOVES; ++i) {
-            if (currentRow + Knight.HORIZONTAL[i] == beginRow && currentCol + Knight.VERTICAL[i] == beginCol) {
-                System.out.println("Inside closedTour and TRUE");
-                return true;
-            }
-        }
-        System.out.println("Inside closedTour and FALSE");
-        return false;
-    }
-
-    public static void printBoard() {
-        for (int i = 0; i < GameBoard.board[0].length; ++i) {
-            for (int j = 0; j < GameBoard.board[1].length; ++j) {
-                System.out.printf("%2d ", GameBoard.board[i][j]);
-               // System.out.println("inside the J loop of print board");
-            }
-            System.out.println();
-        }
-    }
-    private static int play() {
-        int move = 0;
-        do {
-            ++move;
-            GameBoard.board[currentRow][currentCol] = move;
-        //  System.out.println(move);
-        } while (Knight.nextMove());
-       System.out.println("Move should return");
-        return move;
-    }
     public static void main(String[] args) {
+        int ROWS = 8;
+        int COLUMNS = 8;
+        MovementLogic Logic = new MovementLogic();
+        int[][] board = new int[ROWS][COLUMNS];
         int count = 0;
+        int moves;
         for (; count < 10000000; ++count) {
-            currentRow = 0;
-            currentCol = 0;
-            for (int i = 0; i < 8; ++i) {
-                for (int j = 0; j < 8; ++j) {
-                    GameBoard.board[i][j] = 0;
-                //   System.out.println(Arrays.toString(GameBoard.board[i]) + " " + Arrays.toString(GameBoard.board[j]));
+            Logic.currentCol = 0;
+            Logic.currentCol = 0;
+            for (int i = 0; i < ROWS; ++i) {
+                for (int j = 0; j < COLUMNS; ++j) {
+                    board[i][j] = 0;
                 }
             }
-            System.out.println("Before call to play()");
-            moves = play();
-            System.out.println("After call to play()");
+            moves = Logic.play();
             System.out.println(moves);
             if (moves == 64) {
-                if (closedTour(0, 0)) {
+                if (Logic.closedTour(0, 0)) {
                     System.out.println("Closed Tour!");
                 } else {
                     System.out.println("Full Tour!");
                 }
-                printBoard();
+                Logic.printBoard();
                 break;
             }
-            if (count % 1000 == 0) {
-                System.out.println(count / 1000);
+            if (count % 100000 == 0) {
+                System.out.println(count / 100000);
             }
         }
         System.out.println("Trials: " + count);
     }
-
-
 }
 
 
--- ict4315.unit2/src/Knight.java
+++ ict4315.unit2/src/Knight.java
@@ -1,39 +1,7 @@
-/**
- * Created by daverose on 4/2/16.
- */
-import java.util.Random;
-public class Knight {
-    private static int currentRow;
-    private static int currentCol;
-    public static final int MOVES = 8;
-    public static final int[] HORIZONTAL = {2, 1, -1, -2, -2, -1, 1, 2};
-    public static final int[] VERTICAL = {-1, -2, -2, -1, 1, 2, 2, 1};
-    private static Random random = new Random();
 
-    public static boolean nextMove() {
-        int[] moves = {0, 0, 0, 0, 0, 0, 0, 0};
-        int position = 0;
-        for (int i = 0; i < MOVES; ++i) {
-            int r = currentRow + HORIZONTAL[i];
-            int c = currentCol + VERTICAL[i];
-            if (Game.legalMove(r, c)) {
-                moves[position] = i;
-                ++position;
+public class Knight extends Piece {
+    int MOVES = 8;
+    int [] HORIZONTAL = { 2, 1, -1, -2, -2, -1, 1, 2 };
+    int [] VERTICAL = { -1, -2, -2, -1, 1, 2, 2, 1 };
 
-            }
-        }
-        if (position > 0) {
-            int m = random.nextInt(position);
-            currentRow += HORIZONTAL[moves[m]];
-            currentCol += VERTICAL[moves[m]];
-          //  System.out.println("returning TRUE");
-            System.out.println("pos > zero");
-            return true;
-        } else {
-            System.out.println("Knight nextMove is FALSE");
-            return false;
-        }
-
-    }
 }
-
